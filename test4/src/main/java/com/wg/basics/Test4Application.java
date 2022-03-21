c690630a0a374b1faa48b37ec76ca6036055ad71
wg <2418937428@qq.com>
2022-03-21 23:40:40 +0800

    整合SpringMvc静态资源配置-初次提交

---
+++ test4/src/main/java/com/wg/basics/Test4Application.java
@@ -0,0 +1,13 @@
+package com.wg.basics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test4Application.class, args);
+    }
+
+}
