1af8477dbe2f1371812eeaa72dacd2188baef70c
wg <2418937428@qq.com>
2022-03-22 23:55:46 +0800

    Aop初次提交

---
+++ test8/src/main/java/com/wg/basics/Test8Application.java
@@ -0,0 +1,13 @@
+package com.wg.basics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Test8Application.class, args);
+    }
+
+}
