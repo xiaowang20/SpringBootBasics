89e6274301a8d54f12b4218178501d26b981e949
wg <2418937428@qq.com>
2022-03-22 13:50:45 +0800

    整合SpringMvc静态资源配置-第二次提交

---
+++ test4/src/main/java/com/wg/basics/config/MyWebConfig.java
@@ -0,0 +1,19 @@
+package com.wg.basics.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * java代码方式
+ * 实现WebMvcConfigurer接口
+ * 重写addResourceHandlers方法
+ */
+@Configuration
+public class MyWebConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+    }
+}
