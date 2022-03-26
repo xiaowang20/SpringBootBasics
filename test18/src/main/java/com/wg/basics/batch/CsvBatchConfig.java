fd707727fdf396e0b230e7886e46f6176968fdea
wg <2418937428@qq.com>
2022-03-27 00:27:24 +0800

    企业开发之批处理

--- test18/src/main/java/com/wg/basics/batch/CsvBatchConfig.java
+++ test18/src/main/java/com/wg/basics/batch/CsvBatchConfig.java
@@ -1,8 +1,12 @@
 package com.wg.basics.batch;
 
 import com.wg.basics.entiry.User;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
 import org.springframework.batch.item.database.JdbcPagingItemReader;
 import org.springframework.batch.item.file.FlatFileItemReader;
@@ -27,12 +31,15 @@ import javax.sql.DataSource;
  *      2.配置一个step:读写的步骤
  *      3.配置读取csv文件
  *      4.配置写入数据库
+ *  3.本次使用jdbc: 映射关系：cvs文件--->实体类<---sql
  */
 @Configuration
 public class CsvBatchConfig {
 
     @Autowired
     JobBuilderFactory jobBuilderFactory;
+    @Autowired
+    StepBuilderFactory stepBuilderFactory;
     @Autowired
     DataSource dataSource;
 
@@ -70,12 +77,35 @@ public class CsvBatchConfig {
         return reader;
     }
 
+    /**
+     * 写入逻辑
+     * @return
+     */
     @Bean
     JdbcBatchItemWriter itemWriter(){
         JdbcBatchItemWriter<Object> writer = new JdbcBatchItemWriter<>();
-        writer.setDataSource(dataSource);
+        writer.setDataSource(dataSource);//用于注入目的的数据源
+        //用于在写入时执行的查询字符串
         writer.setSql("insert into user(id,username,address,sex)"+"values (:id,:username,:address,:sex)");
+        //将实体类与SQL中占位符一一映射
+        writer.setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>());
+        return writer;
+    }
 
+    @Bean
+    Step csvStep(){
+        return stepBuilderFactory.get("csvStep")//创建一个构建器并初始化其存储库
+                .<User,User>chunk(2)//构建一个步骤，以提供的大小处理块中的项目.以保持读者和作者的类型安全，
+                .reader(itemReader())
+                .writer(itemWriter())
+                .build();
+    }
+
+    @Bean
+    Job csvJob(){
+        return jobBuilderFactory.get("csvJob")
+                .start(csvStep())
+                .build();
     }
 
 }
---
+++ test18/src/main/java/com/wg/basics/controller/HelloController.java
@@ -0,0 +1,30 @@
+package com.wg.basics.controller;
+
+import com.wg.basics.entiry.User;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @Autowired
+    JobLauncher jobLauncher;
+    @Autowired
+    Job job;
+    @GetMapping("/hello")
+    public void hello(){
+        try {
+            jobLauncher.run(job,null);
+        } catch (JobExecutionAlreadyRunningException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
